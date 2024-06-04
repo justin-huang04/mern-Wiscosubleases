cb5b82ecb14cf5ed769bbf37cf0706e31561cd50
justin-huang04 <134019686+justin-huang04@users.noreply.github.com>
2024-06-04 15:23:24 -0500

    Added Placeholders for About page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About UWSubleaseHub
+      </h1>
+      <p className="mb-4 text-slate-700">UWSubleaseHub</p>
+      <p className="mb-4 text-slate-700">Our mission</p>
+      <p className="mb-4 text-slate-700">Contact</p>
+    </div>
+  );
 }
