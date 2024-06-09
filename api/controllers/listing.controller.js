adc81e923f6cb69d893f29db968af8a6f04e8776
justin-huang04 <134019686+justin-huang04@users.noreply.github.com>
2024-06-09 14:38:08 -0500

    Adjusted limit

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -64,7 +64,7 @@ export const getListing = async (req, res, next) => {
 
 export const getListings = async (req, res, next) => {
   try {
-    const limit = parseInt(req.query.limit) || 20;
+    const limit = parseInt(req.query.limit) || 10; // Has to be 1 bigger than showMore in Search.jsx
     const startIndex = parseInt(req.query.startIndex) || 0;
     let priceNegotiable = req.query.priceNegotiable;
 
