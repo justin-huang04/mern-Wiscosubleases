1963d3aeb3a76873016bfc927d1f9da735c99480
justin-huang04 <134019686+justin-huang04@users.noreply.github.com>
2024-06-18 15:08:34 -0500

    Added Build_env

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+const BUILD_ENV = process.env.REACT_APP_BUILD_ENV || "development";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
