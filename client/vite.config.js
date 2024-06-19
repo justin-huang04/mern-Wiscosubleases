df6e9efbee3cad297bfbde9b7eb4f8f4b89a9c2c
justin-huang04 <134019686+justin-huang04@users.noreply.github.com>
2024-06-18 19:06:44 -0500

    Everything back to normal

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-const BUILD_ENV =
-  process.env.REACT_APP_BUILD_ENV || "any-default-local-build_env";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
